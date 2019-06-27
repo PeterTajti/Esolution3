747abee96ca2c88804321dac3e50ebf243bec3a0
Zsolt <zsolt.istenes@gmail.com>
2019-06-27 17:39:06 +0200

    17ora40perc

---
+++ week1/day4/src/MileToKmConverter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MileToKmConverter {
+
+    public static void main(String[] args) {
+        // Write a program that asks for a double that is a distance in miles,
+        // then it converts that value to kilometers and prints it
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a distance in miles!");
+
+        double b = scanner.nextDouble();
+        double c = (b * 1.6);
+
+        System.out.println(c);
+
+    }
+}
+
--- week1/day4/src/SecondsInADay.java
+++ week1/day4/src/SecondsInADay.java
@@ -1,2 +1,18 @@
 public class SecondsInADay {
+
+    public static void main(String[] args) {
+        int currentHours = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+
+        // Write a program that prints the remaining seconds (as an integer) from a
+        // day if the current time is represented by the variables
+
+        int elapsedSeconds = ((currentHours*60*60)+(currentMinutes*25*60)+currentSeconds);
+
+        int remainingSecond = (24*60*60)-elapsedSeconds;
+
+        System.out.println(remainingSecond);
+    }
+
 }
