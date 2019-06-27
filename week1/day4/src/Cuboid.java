ed608097f30f3aa23545c65429532b1c7d4293d0
Zsolt <zsolt.istenes@gmail.com>
2019-06-27 15:58:18 +0200

    16ora

--- week1/day4/src/Cuboid.java
+++ week1/day4/src/Cuboid.java
@@ -8,4 +8,16 @@ public class Cuboid {
 // Surface Area: 600
 // Volume: 1000
 
+        double w = 100.555555;
+        double l = 100.55555;
+        double h = 200.55555;
+
+        double  s = ((w * l) * 2 + (w * h) * 2 + (l * h) * 2);
+        System.out.println(s + "this is the value of the surface");
+
+        double v = (w * l * h);
+        System.out.println(v + "this is the value of the volume");
+    }
 }
+
+
