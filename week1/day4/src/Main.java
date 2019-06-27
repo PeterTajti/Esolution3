b5e086f6a0f52a595a94b30e47cbcfd266975bde
Zsolt <zsolt.istenes@gmail.com>
2019-06-27 12:27:40 +0200

    ebed

--- week1/day4/src/Main.java
+++ week1/day4/src/Main.java
@@ -17,7 +17,20 @@ public class Main {
 
         // Print the remainder of 22 divided by 13
         System.out.println(22 % 13);
+
+
+                // An average Green Fox attendee codes 6 hours daily
+                // The semester is 17 weeks long
+
+                // Print how many hours is spent with coding in a semester by an attendee,
+                // if the attendee only codes on workdays.
+        System.out.println(6*5*17);
+                // Print the percentage of the coding hours in the semester if the average
+                // work hours weekly is 52
+        System.out.println(5*6*100 /52);
+
     }
+
 }
 
 
