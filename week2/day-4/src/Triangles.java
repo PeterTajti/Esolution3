757cc53332f1416981beb7b7acc90aee5a00f64b
Zsolt <zsolt.istenes@gmail.com>
2019-07-14 20:02:31 +0200

    good triangle

--- week2/day-4/src/Triangles.java
+++ week2/day-4/src/Triangles.java
@@ -2,30 +2,40 @@ import javax.swing.*;
 
 import java.awt.*;
 
-import static java.awt.Window.getOwnerlessWindows;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import static javax.swing.JFrame.setDefaultLookAndFeelDecorated;
 
 public class Triangles {
-    public static void mainDraw(Graphics graphics1) {
 
-        triangle(graphics1);
+    public static void mainDraw(Graphics graphics) {
 
-    }
+        int sideSize = 10;
+        int halfSize = sideSize / 2;
+        int rows = 30;
 
-    public static void triangle ( Graphics graphics2) {
-        int number = 34;
-        for (int i = 0; i <= number; i++) {
-            graphics2.drawLine(WIDTH/2-5*i, 0+10*i, (WIDTH/2+(10*number/2)-10*i), 0+(10*number));
-           graphics2.drawLine(WIDTH/2+5*i, 0+10*i,(WIDTH/2-(10*number/2)+10*i), 0+(10*number));
-           graphics2.drawLine(WIDTH/2-5*i, 0+10*i, WIDTH/2+5*i, 0+10*i);
+//        graphics.drawLine(160, 0, 160 + rows * halfSize, rows * sideSize);
+//        graphics.drawLine(160 + rows * halfSize, rows * sideSize, 160 - rows * halfSize, rows * sideSize);
+
+        for (int i = 0; i <= rows / 2; i++) {
+            graphics.drawLine(160 + (sideSize * i), 0 + (sideSize * 2 * i), (160 - rows * halfSize) + (sideSize * 2 * i), rows * sideSize);
+            graphics.drawLine(160 - (sideSize * i), 0 + (sideSize * 2 * i), (160 + rows * halfSize) - (sideSize * 2 * i), rows * sideSize);
+            graphics.drawLine(160 - (sideSize * i), 0 + (sideSize * 2 * i),160 + (sideSize * i),0 + (sideSize * 2 * i));
         }
+
+
     }
 
+
+
+
+
+
+    //
     // Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 320;
 
-    public static void main(String[] args) {
+    public static void main (String[]args){
         JFrame jFrame = new JFrame("Drawing");
         jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         ImagePanel panel = new ImagePanel();
