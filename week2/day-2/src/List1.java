b3c0c596af0f0e368801ca7e4eb04a00904a487c
Zsolt <zsolt.istenes@gmail.com>
2019-07-23 16:02:29 +0200

    cheking data structures

--- week2/day-2/src/List1.java
+++ week2/day-2/src/List1.java
@@ -1,32 +1,6 @@
-//Create an empty list which will contain names (strings)
-//Print out the number of elements in the list
-//Add William to the list
-//Print out whether the list is empty or not
-//Add John to the list
-//Add Amanda to the list
-//Print out the number of elements in the list
-//Print out the 3rd element
-//Iterate through the list and print out each name
-//William
-//John
-//Amanda
-//Iterate through the list and print
-//1. William
-//2. John
-//3. Amanda
-//Remove the 2nd element
-//Iterate through the list in a reversed order and print out each name
-//Amanda
-//William
-//Remove all elements
-
 import java.util.ArrayList;
-
 public class List1 {
-
     public static void main(String... args) {
-
-
 
         ArrayList <String> stringek = new ArrayList<>();
         System.out.println(stringek);
@@ -48,11 +22,6 @@ public class List1 {
         for (int i = 0; i < stringek.size(); i++)
         System.out.println(stringek.get(i));
 
-//Iterate through the list and print
-//1. William
-//2. John
-//3. Amanda
-
         int a = 1;
 
         for (int i = 0; i < stringek.size(); i++) {
@@ -60,29 +29,16 @@ public class List1 {
             a++;
         }
 
-
-//Remove the 2nd element
-//Iterate through the list in a reversed order and print out each name
-//Amanda
-//William
-
-
         stringek.remove (1);
 
         for (int i = stringek.size() -1; i >= 0; i--) {
             System.out.println(stringek.get(i));
         }
 
-
         stringek.clear();
         System.out.println(stringek);
-
-
-
     }
-
-
-    }
+}
 
 
 
