a584b5656d93b7bc299576b9b6d105dfc423ce61
Zsolt <zsolt.istenes@gmail.com>
2019-07-26 20:25:34 +0200

    learning the ugly MostCommonChar...

--- week5/w5d2/src/MostCommonCharacters/MostCommonCharacters.java
+++ week5/w5d2/src/MostCommonCharacters/MostCommonCharacters.java
@@ -1,78 +1,71 @@
-//package MostCommonCharacters;
-//
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.util.*;
-//
-//public class MostCommonCharacters {
-//
-//
-//
-//    public static void main(String[] args) {
-//        static String targetFile = "countchar.txt";
-//        theMostCommonCharacterInThisFile(targetFile);
-//
-//    }
-//
-//    private static void theMostCommonCharacterInThisFile(String filePath) {
-//
-//        ArrayList<String> list = new ArrayList<>();
-//
-//        int i = 1;
-//
-//        try {
-//            File file = new File(filePath);
-//            Scanner scanner = new Scanner(file);
-//            while (scanner.hasNextLine()) {
-//                list.add(i - 1, scanner.nextLine());
-//                i++;
-//            }
-//            scanner.close();
-//        } catch (FileNotFoundException e) {
-//            System.out.println("File does not exist!");
-//        }
-//
-//        ArrayList<Character> kar = new ArrayList<>();
-//        String temp;
-//
-//        for (int j = 0; j < list.size(); j++) {
-//            temp = list.get(j);
-//            char[] wut = temp.toCharArray();
-//            for (int k = 0; k < wut.length; k++) {
-//                kar.add(k, wut[k]);
-//            }
-//        }
-//
-//        HashMap<Character, Integer> mcc = new HashMap<>();
-//
-//        // counts the values based on same keys
-//
-//        for (i = 0; i < kar.size(); i++) {
-//
-//            if (mcc.containsKey(kar.get(i))) {
-//                mcc.put(kar.get(i), mcc.get(kar.get(i)) + 1);
-//            } else {
-//                mcc.put(kar.get(i), 1);
-//            }
-//        }
-//
-//        System.out.println(mcc);
-//
-//        Map.Entry<Character, Integer> maxEntry = null;
-//        Map.Entry<Character, Integer> secondMaxEntry = null;
-//
-//        for (Map.Entry<Character, Integer> entry : mcc.entrySet()) {
-//            if (maxEntry == null || entry.getValue() > maxEntry.getValue()) {
-//                maxEntry = entry;
-//            }
-//            if (secondMaxEntry == null || entry.getValue() > secondMaxEntry.getValue() && entry.getValue() != maxEntry.getValue()) {
-//                secondMaxEntry = entry;
-//            }
-//        }
-//
-//        System.out.println("\"" + maxEntry.getKey() + "\" " + ": " + maxEntry.getValue() + ",");
-//        System.out.println("\"" + secondMaxEntry.getKey() + "\" " + ": " + secondMaxEntry.getValue());
-//
-//    }
-//
-//}
+package MostCommonCharacters;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+
+public class MostCommonCharacters {
+
+    public static void main(String[] args) {
+      mostCommonChar("C:\\Users\\izs\\greenfox\\isteneszsolt\\week5\\w5d2\\src\\MostCommonCharacters\\countchar.txt");
+    }
+
+    public static void mostCommonChar (String fileName){
+      List<String> file = readFile(fileName);
+
+      String singleLineFile = "";  //ez azt jelenti, hogy üresként kezd
+
+      for (String line : file) {
+        singleLineFile += line;
+      }
+      //System.out.println(file.toString());
+      //System.out.println(singleLineFile);
+
+      char [] newCharArray = singleLineFile.toCharArray();
+
+      String [] newNewStringArray = new String[singleLineFile.length()];
+
+      newNewStringArray = singleLineFile.split("");
+
+      //egyik verzió
+      HashMap <Character, Integer> countedCharactersSecond = new HashMap<>();
+
+      for (int i = 0; i < singleLineFile.length(); i++) {
+        Character singleChar = singleLineFile.charAt(i);
+
+        if (countedCharactersSecond.containsKey(singleLineFile.charAt(i))) {
+          countedCharactersSecond.put(singleLineFile.charAt(i), countedCharactersSecond.get(singleLineFile.charAt(i))+1);
+
+        } else {
+          countedCharactersSecond .put(singleLineFile.charAt(i), 1);
+        }
+      }
+
+
+      //másik verzió
+      HashMap<String, Integer> countedCharacters = new HashMap<>();
+
+      for (String character : newNewStringArray) {
+        if (countedCharacters.containsKey(character)) {
+          countedCharacters.put(character, countedCharacters.get(character)+1);
+
+        } else {
+          countedCharacters.put(character, 1);
+        }
+      }
+
+    }
+
+    public static List<String> readFile (String fileName) {
+       try {
+         Path filePath = Paths.get ("C:\\Users\\izs\\greenfox\\isteneszsolt\\week5\\w5d2\\src\\MostCommonCharacters\\countchar.txt");
+         List<String> lines = Files.readAllLines(filePath);
+         return lines;
+       } catch (Exception e) {
+         System.out.println("Oops, cant read the file");
+       }
+       return null;
+    }
+}
