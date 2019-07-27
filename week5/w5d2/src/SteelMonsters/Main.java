a5a24ac4109ced1add354068510721bfda980955
Zsolt <zsolt.istenes@gmail.com>
2019-07-27 18:40:14 +0200

    adding getter, tank repair and upgrade

--- week5/w5d2/src/SteelMonsters/Main.java
+++ week5/w5d2/src/SteelMonsters/Main.java
@@ -5,10 +5,17 @@ public class Main {
     Tank Panther = new Tank ("Panther", 500, 75);
     Tank Tiger = new Tank ("Tiger", 750, 88);
 
-      Panther.attack(Tiger, 10);
+      Panther.attack(Tiger, 3);
+      Panther.getStats();
+      Tiger.getStats();
 
+      Panther.repair(3);
+      Panther.upgradeWeapon(3);
 
+      Panther.getStats();
+      Tiger.getStats();
 
+      Panther.attack(Tiger, 6);
 
   }
 }
--- week5/w5d2/src/SteelMonsters/Tank.java
+++ week5/w5d2/src/SteelMonsters/Tank.java
@@ -12,17 +12,35 @@ public class Tank {
     this.firePower = firePower;
   }
 
+  public void getStats () {
+    System.out.println(this.name + "'s stats: " + this.healthPoints + " healthpoints " + " & " + this.firePower + " firepower."+"\n");
+  }
+
+  public void repair (int numberOfRepairs) {
+    for (int j = 0; j < numberOfRepairs; j++) {
+      this.healthPoints += 100;
+      }
+    System.out.println("\n" + "We repaired " + this.name + " for " + (numberOfRepairs) + "*100" + " healthpoints" + "\n");
+  }
+
+  public void upgradeWeapon (int numberOfUpgrades) {
+    for (int i = 0; i < numberOfUpgrades; i++) {
+      this.firePower +=5;
+    }
+    System.out.println("\n" + "We upgraded " + this.name + " for " + (numberOfUpgrades) + "*5" + " firepower" + "\n");
+  }
+
   public void attack (Tank targetTankName, int numberOfAttacks) {
 
-    for (int i = 1; i < numberOfAttacks; i++) {
+    for (int i = 1; i < numberOfAttacks+1; i++) {
 
-//      if (this.healthPoints <= 0) {
-//        System.out.println("No more attacks! " + this.name + " has been destroyed!");
-//      }
-//
-//      if (targetTankName.healthPoints <= 0) {
-//        System.out.println("No more attacks! " + targetTankName.name + " has been destroyed!");
-//      }
+      if (this.healthPoints <= 0) {
+        System.out.println("No more attacks! " + this.name + " has been destroyed!");
+      }
+
+      if (targetTankName.healthPoints <= 0) {
+        System.out.println("No more attacks! " + targetTankName.name + " has been destroyed!");
+      }
 
       if (this.healthPoints <= 0) {
         System.out.println("\n" + this.name + " is destroyed! " + "\n" + targetTankName.name + " survived the battle.");
@@ -49,6 +67,15 @@ public class Tank {
               this.name + " suffered " + targetTankName.firePower + " damage, and it has " +
                       this.healthPoints + " healthpoints left." + "\n" + " ");
 
+      if (this.healthPoints <= 0) {
+        System.out.println("\n" + this.name + " is destroyed! " + "\n" + targetTankName.name + " survived the battle.");
+        break;
+      }
+
+      if (targetTankName.healthPoints <= 0) {
+        System.out.println("\n" + targetTankName.name + " is destroyed! " + " \n" + this.name + " survived the battle.");
+        break;
+      }
 
     }
   }
