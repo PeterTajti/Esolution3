0833446572c4639d7b1a02f0dc00a078dff0047c
Zsolt <zsolt.istenes@gmail.com>
2019-07-29 23:14:30 +0200

    file io doubled

--- week3/w3d2/src/Births.java
+++ week3/w3d2/src/Births.java
@@ -6,34 +6,69 @@
 //
 //    // You can find such a CSV file in this directory named births.csv
 //    // If you pass "births.csv" to your function, then the result should be either 2006, or 2016.
-
-package Births;
-
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
 
 public class Births {
+  public static void main(String[] args) {
 
-
-  public static void mostBirths (String fileName) {
-
-    List<String > file = readFile(fileName);
-
+  //String fileName = "births.csv";
+  mostBirthYears("births.csv");
   }
 
 
+  public static void mostBirthYears (String fileName) {
 
-  public static List<String> readFile (String fileName) {
+    String [] parts = {};
+    ArrayList<String> list = new ArrayList<>();
+
+    String [] dates = {};
+    ArrayList<String> years = new ArrayList<>();
+
+    HashMap<String, Integer> myHashMap = new HashMap<>();
+
     try {
-      Path filePath = Paths.get ("C:\\Users\\izs\\greenfox\\isteneszsolt\\week3\\w3d3\\src\\Births\\births.csv");
+      Path filePath = Paths.get("births.csv");
       List<String> lines = Files.readAllLines(filePath);
-      return lines;
+
+      for (int i = 0; i < list.size(); i++) {
+        parts = lines.get(i).split(";");
+        list.add(parts[1]);
+      }
+
+      for (int i = 0; i < list.size(); i++) {
+        dates = lines.get(i).split("-");
+        years.add(dates[0]);
+      }
+
+
+      int counter = 1;
+
+      for (int i = 0; i < years.size(); i++) {
+        if (myHashMap.containsKey(years.get(i))){
+          myHashMap.put(years.get(i), myHashMap.get(years.get(i))+1);
+        } else {
+          myHashMap.put(years.get(i), counter);
+        }
+      }
+
+      System.out.println(years);
+      System.out.println(myHashMap);
+
+      int max = Collections.max(myHashMap.values());
+      for (String key : myHashMap.keySet()) {
+        if (myHashMap.get(key) == max) {
+          System.out.println("years with most births: " + key);
+        }
+      }
     } catch (Exception e) {
-      System.out.println("Oops, cant read the file");
+      System.out.println("Cannot read the file!");
     }
-    return null;
+
   }
+
 
 }
---
+++ week3/w3d2/src/Doubled/Doubled.java
@@ -0,0 +1,53 @@
+package Doubled;
+
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Doubled {
+
+  public static void main(String[] args) {
+    rewriteFile("C:\\Users\\izs\\greenfox\\isteneszsolt\\week3\\w3d2\\src\\Doubled\\duplicated-chars.txt");
+  }
+
+
+  public static List<String> decryption (String filename) {
+    List<String> lines = ReadFile.readFile(filename);
+    if(lines == null){
+      return new ArrayList<>();
+    }
+    List<String> decryptedLines = new ArrayList<>();
+    String decryptedLine = "";
+
+    for (int i = 0; i < lines.size(); i++) {
+      for (int j = 0; j < lines.get(i).length(); j += 2) {
+        decryptedLine += lines.get(i).charAt(j);
+      }
+      decryptedLines.add(decryptedLine);
+      decryptedLine = "";
+    }
+    return decryptedLines;
+  }
+
+
+
+
+  public static void rewriteFile (String filename){
+    try{
+      Path filePath = Paths.get(filename);
+      if(Files.exists(filePath)){
+        List<String> decryptedLines = decryption(filename);
+        Files.write(filePath,decryptedLines);
+        System.out.println("The decryption was successful!");
+      } else {
+        throw new Exception();
+      }
+    } catch (Exception ex){
+      System.out.println("Unable to read the file: " + filename);
+    }
+  }
+
+}
