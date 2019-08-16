59ba43b5d0a496141cc999b03e1f267d74b82f7e
Zsolt <zsolt.istenes@gmail.com>
2019-08-16 12:15:52 +0200

    fox fox fox...:D

--- week7/w7d4/src/main/java/com/myfoxclub/myfox/controllers/MainController.java
+++ week7/w7d4/src/main/java/com/myfoxclub/myfox/controllers/MainController.java
@@ -1,5 +1,7 @@
 package com.myfoxclub.myfox.controllers;
 
+import com.myfoxclub.myfox.models.Fox;
+import com.myfoxclub.myfox.models.FoxPack;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +12,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
 
   @GetMapping("/")
-  public String home(@RequestParam(name = "name") String name, Model model){
-    model.addAttribute("name", name);
-    return "index";
+  public String home(@RequestParam(name = "name") String name, Model model) {
+
+    String route = "login";
+    model.addAttribute("nameNotUsed", "You have provided a name that has not been used before, add it as a new one!");
+
+    FoxPack myFoxPack = new FoxPack();
+    for (int i = 0; i < myFoxPack.getFoxList().size(); i++) {
+      Fox foxTemp = myFoxPack.getFoxList().get(i);
+      if (foxTemp.getName().equals(name)) {
+        model.addAttribute("fox", foxTemp);
+        route = "index";
+      }
+    }
+
+    return route;
   }
 
   @GetMapping("/login")
-  public String login(){
+  public String login(Model model) {
+    model.addAttribute("nameNotUsed", null);
     return "login";
   }
 
   @PostMapping("/login")
-  public String login2 (@RequestParam("name") String name) {
+  public String login2(@RequestParam("name") String name) {
     return "redirect:/?name=" + name;
   }
 
+  @GetMapping("/nutritionstore")
+  public String nutritionStore() {
+    return "nutritionStore";
+  }
 
 }
